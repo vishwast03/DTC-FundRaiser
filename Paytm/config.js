248705ad5c4d559f6ea5ad7df00bb30adfb0dbd0
Vishwas Tyagi <vishwast8126@gmail.com>
2021-06-11 13:08:56 +0530

    First Working App

---
+++ Paytm/config.js
@@ -0,0 +1,6 @@
+var PaytmConfig = {
+    mid: "XYMPwY27019154848359",
+    key: "zSWERUSAqKGSt_d7",
+    website: "WEBSTAGING",
+  };
+  module.exports.PaytmConfig = PaytmConfig;
--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const path = require("path");
 const https = require("https");
 const qs = require("querystring");
 
+const checksum_lib = require("./Paytm/checksum");
+const config = require("./Paytm/config");
+
 const app = express();
 
 const parseUrl = express.urlencoded({ extended: false });
@@ -16,13 +19,13 @@ app.use('/static', express.static('static'));
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', (req, res)=>{
-  const params = { };
+app.get('/', (req, res) => {
+  const params = {};
   res.status(200).render('index.pug', params);
 });
 
-app.get('/donate', (req, res)=>{
-  const params = { };
+app.get('/donate', (req, res) => {
+  const params = {};
   res.status(200).render('donate.pug', params);
 });
 
@@ -32,17 +35,17 @@ app.post("/paynow", [parseUrl, parseJson], (req, res) => {
     customerId: req.body.name,
     customerEmail: req.body.email,
     customerPhone: req.body.phone
-}
-if(!paymentDetails.amount || !paymentDetails.customerId || !paymentDetails.customerEmail || !paymentDetails.customerPhone) {
+  }
+  if (!paymentDetails.amount || !paymentDetails.customerId || !paymentDetails.customerEmail || !paymentDetails.customerPhone) {
     res.status(400).send('Payment failed')
-} 
-else {
+  }
+  else {
     var params = {};
     params['MID'] = config.PaytmConfig.mid;
     params['WEBSITE'] = config.PaytmConfig.website;
     params['CHANNEL_ID'] = 'WEB';
     params['INDUSTRY_TYPE_ID'] = 'Retail';
-    params['ORDER_ID'] = 'TEST_'  + new Date().getTime();
+    params['ORDER_ID'] = 'TEST_' + new Date().getTime();
     params['CUST_ID'] = paymentDetails.customerId;
     params['TXN_AMOUNT'] = paymentDetails.amount;
     params['CALLBACK_URL'] = 'http://localhost:3000/callback';
@@ -51,20 +54,90 @@ else {
 
 
     checksum_lib.genchecksum(params, config.PaytmConfig.key, function (err, checksum) {
-        var txn_url = "https://securegw-stage.paytm.in/theia/processTransaction"; // for staging
-        // var txn_url = "https://securegw.paytm.in/theia/processTransaction"; // for production
+      var txn_url = "https://securegw-stage.paytm.in/theia/processTransaction";
 
-        var form_fields = "";
-        for (var x in params) {
-            form_fields += "<input type='hidden' name='" + x + "' value='" + params[x] + "' >";
-        }
-        form_fields += "<input type='hidden' name='CHECKSUMHASH' value='" + checksum + "' >";
+      var form_fields = "";
+      for (var x in params) {
+        form_fields += "<input type='hidden' name='" + x + "' value='" + params[x] + "' >";
+      }
+      form_fields += "<input type='hidden' name='CHECKSUMHASH' value='" + checksum + "' >";
 
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><head><title>Merchant Checkout Page</title></head><body><center><h1>Please do not refresh this page...</h1></center><form method="post" action="' + txn_url + '" name="f1">' + form_fields + '</form><script type="text/javascript">document.f1.submit();</script></body></html>');
-        res.end();
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write('<html><head><title>Merchant Checkout Page</title></head><body><center><h1>Please do not refresh this page...</h1></center><form method="post" action="' + txn_url + '" name="f1">' + form_fields + '</form><script type="text/javascript">document.f1.submit();</script></body></html>');
+      res.end();
     });
-}
+  }
+});
+
+app.post("/callback", (req, res) => {
+  // Route for verifiying payment
+
+  var body = '';
+
+  req.on('data', function (data) {
+     body += data;
+  });
+
+   req.on('end', function () {
+     var html = "";
+     var post_data = qs.parse(body);
+
+     // received params in callback
+     console.log('Callback Response: ', post_data, "\n");
+
+
+     // verify the checksum
+     var checksumhash = post_data.CHECKSUMHASH;
+     // delete post_data.CHECKSUMHASH;
+     var result = checksum_lib.verifychecksum(post_data, config.PaytmConfig.key, checksumhash);
+     console.log("Checksum Result => ", result, "\n");
+
+
+     // Send Server-to-Server request to verify Order Status
+     var params = {"MID": config.PaytmConfig.mid, "ORDERID": post_data.ORDERID};
+
+     checksum_lib.genchecksum(params, config.PaytmConfig.key, function (err, checksum) {
+
+       params.CHECKSUMHASH = checksum;
+       post_data = 'JsonData='+JSON.stringify(params);
+
+       var options = {
+         hostname: 'securegw-stage.paytm.in', // for staging
+         // hostname: 'securegw.paytm.in', // for production
+         port: 443,
+         path: '/merchant-status/getTxnStatus',
+         method: 'POST',
+         headers: {
+           'Content-Type': 'application/x-www-form-urlencoded',
+           'Content-Length': post_data.length
+         }
+       };
+
+
+       // Set up the request
+       var response = "";
+       var post_req = https.request(options, function(post_res) {
+         post_res.on('data', function (chunk) {
+           response += chunk;
+         });
+
+         post_res.on('end', function(){
+           console.log('S2S Response: ', response, "\n");
+
+           var _result = JSON.parse(response);
+             if(_result.STATUS == 'TXN_SUCCESS') {
+                 res.send('payment sucess')
+             }else {
+                 res.send('payment failed')
+             }
+           });
+       });
+
+       // post the data
+       post_req.write(post_data);
+       post_req.end();
+      });
+     });
 });
 
 app.listen(PORT, () => {
